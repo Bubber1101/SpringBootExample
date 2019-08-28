95475aaf692c21a33851dd663aef4773c4c6b714
Patryk Stradomski <patrykstradomski@wp.pl>
2019-08-28 22:29:58 +0200

    Added Checkstyle

--- src/main/java/dev/estimate/springtut/SpringtutApplication.java
+++ src/main/java/dev/estimate/springtut/SpringtutApplication.java
@@ -2,12 +2,14 @@ package dev.estimate.springtut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringtutApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringtutApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtutApplication.class, args);
+    }
 
 }
